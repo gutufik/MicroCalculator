8cb6091dd131211fc24e09e313fa3b99c634feb3
201913 <201913@1220-09>
2021-06-25 09:32:53 +0300

    Create Main form

--- MicroCalc/MainWindow.xaml.cs
+++ MicroCalc/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace MicroCalc
     /// </summary>
     public partial class MainWindow : Window
     {
+        int loan;
+        int daysCount;
+        float[] percents;
         public MainWindow()
         {
+            int loan = 0;
+            int daysCount = 0;
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            loan = int.Parse(tbLoan.Text);
+            daysCount = int.Parse(tbDays.Text);
+            percents = new float[daysCount];
+
+
+            var perc = tbPercents.Text.Split(' ');
+            for (int i = 0; i < daysCount; ++i)
+            {
+                percents[i] = int.Parse(perc[i]) / 10;
+            }
+
+            tbItogSum.Text = "grgr \n gg";
+
         }
     }
 }
